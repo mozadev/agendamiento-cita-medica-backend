19eb42bede4f5c8cbda31509037a080c495df3d6
mozadev <ceosmore@gmail.com>
2025-11-08 19:40:07 -0500

    chore: configuración inicial del proyecto
    - package.json con dependencias
    - tsconfig.json para TypeScript
    - jest.config.js para tests
    - .gitignore configurado

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/infrastructure/**/*.ts', // Excluir infraestructura (requiere tests de integración)
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  verbose: true,
+};
+
